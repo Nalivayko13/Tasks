79345bbac85c8a75cad85525e5f88843c9674044
Polina <nalivayko.popalina@gmail.com>
2021-11-24 01:52:11 +0300

    feat: add func Create4Point

--- Parallelogram.go
+++ Parallelogram.go
@@ -1,7 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
 type Parallelogram struct {
 	p1, p2, p3, p4 Point
+}
+func Create4Point(a,b,c Point) Point{
+	var d Point
+	r1:=math.Sqrt(math.Pow(float64((a.x - b.x)),2)+math.Pow(float64((a.y-b.y)),2))
+	r2:=math.Sqrt(math.Pow(float64((b.x - c.x)),2)+math.Pow(float64((b.y-c.y)),2))
+	r3:=math.Sqrt(math.Pow(float64((a.x - c.x)),2)+math.Pow(float64((a.y-c.y)),2))
+	max:=math.Max(r1, math.Max(r2,r3))
+	switch max {
+	case r1:
+		center:=FindCenter(a,b)
+		d.x=2*center.x-c.x
+		d.y=2*center.y-c.y
+	case r2:
+		center:=FindCenter(b,c)
+		d.x=2*center.x-a.x
+		d.y=2*center.y-a.y
+	case r3:
+		center:=FindCenter(a,c)
+		d.x=2*center.x-b.x
+		d.y=2*center.y-b.y
+	default:
+		fmt.Println("oh nooo")
+	}
+	return d
+}
+
+func FindCenter(p1, p2 Point) Point{
+	var center Point
+	center.x=(p1.x+p2.x)/2
+	center.y=(p1.y+p2.y)/2
+	return center
 }
 
 func (pam Parallelogram) CalcSquare() float64{
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	WHITE= iota
 	BLACK
@@ -14,14 +16,21 @@ const (
 type Color byte
 
 func main() {
-	field:=CreateField(5)
+	field:=CreateField(7)
 
-	p1:=CreatePoint(4,2,1)
-	p2:=CreatePoint(2,0,1)
-	p3:=CreatePoint(1,3,1)
+	p1:=CreatePoint(2,2,1)
+	p2:=CreatePoint(2,5,1)
+	p3:=CreatePoint(4,1,1)
 
 	field.SetThreePoints(p1,p2,p3)
 	field.PrintField()
+
+	newP:=Create4Point(p1,p2,p3)
+	fmt.Println(newP.x, newP.y)
+	field.f[newP.x][newP.y]=1
+	field.PrintField()
+
+
 
 
 }
